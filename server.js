dceeb5a1c7f88ec2aa44d083e5fab4feb776ded2
Anup moyal <anupmoyal@gmail.com>
2025-06-29 16:31:13 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/db.js";
+import authRoutes from "./src/routes/auth.js";
+import contactRoutes from "./src/routes/contact.routes.js";
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api", contactRoutes);
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+console.log("jai shree ram");
+
+});
+
+
+
